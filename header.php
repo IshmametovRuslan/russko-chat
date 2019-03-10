92d0430e17d07a7c444548fabc63f459d68691f9
Ruslan <russ-i@yandex.ru>
2019-03-10 14:15:53 +0300

    add header styles

--- header.php
+++ header.php
@@ -5,7 +5,11 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="styles\style.css">
 	<title>Russko-Chat</title>
 </head>
 <body>
-
+<header>
+	<div class="page-title">Russko-Chat</div>
+	<div class="user-block"></div>
+</header>
